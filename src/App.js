c64d1a01f30d2ba9ed49c6c34a80eca105fe0123
kennedybryant <kennedy_marisa96@yahoo.com>
2021-08-13 13:33:36 -0400

    App and Character Structured

--- src/App.js
+++ src/App.js
@@ -1,17 +1,50 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import axios from 'axios';
+import Character from './components/Character';
+import Card from './components/Card';
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
 
+  const [starWarsData, setStarWarsData] = useState([])
+  const [currentCharacter, setCurrentCharacter] = useState()
+
+  const openCard = name => {
+    setCurrentCharacter(name)
+  }
+
+  const closeCard = () => {
+    setCurrentCharacter(null)
+  }
+
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
 
+  useEffect(() => {
+    axios.get('https://swapi.dev/api/people')
+      .then(res => {
+        console.log(res.data)
+        setStarWarsData(res.data)
+      })
+      .catch(err => {
+        console.error(err)
+      })
+  }, [])
+
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
+      {
+        starWarsData.map(chara => {
+          return <Character key = {chara.name} data={chara} action={openCard}/>
+        })
+      }
+      {
+        currentCharacter && <Card charaName={currentCharacter} close={closeCard} />
+      }
     </div>
   );
 }
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,28 @@
 // Write your Character component here
+import React from 'react';
+
+export default function Character({ data, action }) {
+
+    // return data.map((props) => 
+    //     <div className='container'>
+    //         <h3>Name: {props.name}</h3>
+    //         <p>Birthyear: {props.birth_year}</p>
+    //         <p>Gender: {props.gender}</p>
+    //         <p>Mass: {props.mass}</p>
+    //         <p>Height: {props.height}</p>
+    //         <p>Hair Color: {props.hair_color}</p>
+    //         <p>Skin Color: {props.skin_color}</p>
+    //         <p>Eye Color: {props.eye_color}</p>
+    //     </div>
+    // )
+
+    return (
+        <div className='character'>
+            {data.name}
+            <button onClick={() => action(data.name)}>
+                Character Information
+            </button>
+        </div>
+    )
+
+}
